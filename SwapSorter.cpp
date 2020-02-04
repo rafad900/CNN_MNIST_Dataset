0774fc5a85a90d4b64da95358eaad2fc2eee1ef8
rafaeldiaz <rafad900@gmail.com>
2020-02-03 23:13:14 -0800

    I have added the DFS and the goal function. Also added a neighbors and modified the swap_section fucntion

--- SwapSorter.cpp
+++ SwapSorter.cpp
@@ -1,7 +1,11 @@
+#include <algorithm>
 #include "SwapSorter.hpp"
 #include "permToInt.cpp"
 
-SwapSorter::SwapSorter(std::vector<int> user_input): numbers(user_input) {} //{ numbers = user_input; } Im using a initilizer list just if you wondering
+SwapSorter::SwapSorter(std::vector<int> user_input): numbers(user_input) {
+	sorted_numbers = numbers; 
+	std::sort(sorted_numbers.begin(), sorted_numbers.end());
+} //{ numbers = user_input; } Im using a initilizer list just if you wondering
 
 void SwapSorter::print() {							// Just a random print function, mainly for debugging
 	std::cout << "This is the original sequence: [";
@@ -18,7 +22,7 @@ void SwapSorter::print() {							// Just a random print function, mainly for deb
 	}
 }
 	
-std::vector<int> SwapSorter::swap_section(int l, int r) {
+std::vector<int> SwapSorter::swap_section(std::vector<int> parent, int l, int r) {
 	std::vector<int> swapped_vector (numbers.size(), 0);
 	for (int i = 0; i <  l; i++) {					// To fill anything before the section to be swapped
 		swapped_vector[i] = numbers[i];
@@ -33,10 +37,36 @@ std::vector<int> SwapSorter::swap_section(int l, int r) {
 }
 
 std::vector<int> SwapSorter::sort() {
-	for (int a = 0; a < numbers.size(); a++) {			// Should perform swaps from left to right
-		for (int b = numbers.size()-1; b >= 0; b--) {	// Should perform swaps from right to left
-			swaps.push_back(swap_section(a, b));
+	std::vector<int> result = {0};
+	return result;// This should call the bbbIDS and BFS 
+}
+
+std::vector< std::vector<int> > SwapSorter::get_neighbors( std::vector<int> parent) {
+	std::vector< std::vector<int> > neighbors;
+	for (int a = 0; a < parent.size(); a++) {			// Should perform swaps from left to right
+		for (int b = parent.size()-1; b >= 0; b--) {	// Should perform swaps from right to left
+			neighbors.push_back(swap_section(parent, a, b));
 		}
 	}
-	return numbers;
+	return neighbors;
+}
+
+bool SwapSorter::is_goal(std::vector<int> perm) {
+	return perm == sorted_numbers;
+}
+
+bool SwapSorter::DFS(std::vector<int> perm, int depth) {
+	if (is_goal(perm)) 
+		return true;
+	if (depth == 0) 
+		return false;
+	std::vector< std::vector<int> > neighbors = get_neighbors(perm);
+	for (std::vector<int> n: neighbors) {
+		int index = permToInt(perm);
+		if (!visited[index]) {
+			visited[index] = true;
+			return DFS(n, depth-1);
+		}
+	}
+	return false;
 }
--- SwapSorter.hpp
+++ SwapSorter.hpp
@@ -6,12 +6,16 @@
 class SwapSorter {
 	public:
 		std::vector<int> sort();
-		std::vector<int> swap_section(int l, int r);
+		std::vector<int> swap_section(std::vector<int> perm, int l, int r);
 		SwapSorter(std::vector<int> user_input);
 		void print();
 		
 	private:
+		bool is_goal(std::vector<int> perm);
+		std::vector< std::vector<int> > get_neighbors( std::vector<int> parent);
+		bool DFS(std::vector<int> perm, int depth);
 		std::vector<int> numbers;
+		std::vector<int> sorted_numbers;
 		std::vector< std::vector<int> > swaps;
 		std::vector<bool> visited;
 };
