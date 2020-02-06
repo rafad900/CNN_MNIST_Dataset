f4e11db9d4374293a78a77725716355cf8efabae
unknown <bipinregmi.official@gmail.com>
2020-02-06 14:12:38 -0800

    slight changes in main()

--- main.cpp
+++ main.cpp
@@ -6,35 +6,36 @@
 
 int main() {
 	std::vector<int> user_input;
-	std::vector<int> output;
-	std::cout << "Give me a number: ";
 	std::string input;
-
-	while (getline(std::cin, input)) {
-		if (input.empty()) {
-			std::cout << "Sorting then\n\n";
-			break;
-		}
-		user_input.push_back(std::stoi(input));
-		std::cout << "Another number? ";
+	std::cout << "Enter a list of numbers with space in between them: ";
+	getline(cin, input);
+	std::istringstream iss(input);
+	int temp;
+	while(iss >> temp)
+	{
+		user_input.push_back(temp);
 	}
-
 	SwapSorter *sorter = new SwapSorter(user_input);
-    double start_time = clock();
+	std::cout<<"Start BFS:"<<std::endl;
+	double start_time = clock();
 	sorter->BFSsort();
-    double finish_time = clock();
-    double time = ((finish_time - start_time)/ CLOCKS_PER_SEC );
-    std::cout<< "Time Taken by BFS: "<<time<<" sec"<<std::endl;
-
-    start_time = clock();
-    sorter->BFSsort();
-    finish_time = clock();
-    time = ((finish_time - start_time)/ CLOCKS_PER_SEC );
-    std::cout<< "Time Taken by IDS: "<<time<<" sec"<<std::endl;
+	double finish_time = clock();
+	double time = ((finish_time - start_time)/ CLOCKS_PER_SEC );
+	std::cout<< "Time Taken by BFS: "<<time<<" sec"<<std::endl;
 	delete sorter;
+
+	SwapSorter *sorter_ = new SwapSorter(user_input);
+	std::cout<<"\nStart IDS:"<<std::endl;
+	start_time = clock();
+	sorter_->IDSsort();
+	finish_time = clock();
+	time = ((finish_time - start_time)/ CLOCKS_PER_SEC );
+	std::cout<< "Time Taken by IDS: "<<time<<" sec"<<std::endl;
+	delete sorter_;
 
 
 	std::cout << std::endl;
 	return 0;
+
 }
 
