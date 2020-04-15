06a1d70ef35668a5e45e0c20ff7e52c83d3a175f
rafaeldiaz <rafad900@gmail.com>
2020-04-15 14:42:28 -0700

    Here is the latest

--- Project4/neuron.py
+++ Project4/neuron.py
@@ -3,6 +3,8 @@ import os
 import random
 
 class Neuron:
+
+    # SELF IN THE FUNCTION AND STUFF JUST MEANS THAT IT BELONGS TO THE CLASS 
     def __init__(self):
         self.feature_file = 0
         self.weights = []
@@ -18,37 +20,50 @@ class Neuron:
             self.feature_file = open('seven_and_nine_features.txt', 'r')
         for i in range(7):
             self.weights.append(random.uniform(-0.1, 0.1))
+        print("These are the starting weights: ", end='')
+        print(self.weights)
+        print()
         
     def train(self):
         for a in range(1000): # 1000 epochs according to pdf
+            if (a%2 == 0):
+                print("These are the weigths every two epochs: ", end='')
+                print(self.weights)
             for line in self.feature_file:
                 features = line.split(',')
                 result = self.predict(features)
                 if (result != int(features[-1])):
                     self.adjust_weights(result, features)
+            #print("These are the weights at the end of iteration: ", end='')
+            #print(self.weights)
+            self.feature_file.seek(0)
 
     def adjust_weights(self, result, features):
         y, t = 0, 0
         if (int(features[-1]) == 9):
             t = 1
-        if (int(features[-1]) == 9):
-            y == 1
+        if (result == 9):
+            y = 1
         w0 = self.weights[0] - 0.1*(y - t) * -1
-        w1 = self.weights[1] - 0.1*(y - t) * features[0]
-        w2 = self.weights[2] - 0.1*(y - t) * features[1]
-        w3 = self.weights[3] - 0.1*(y - t) * features[2]
-        w4 = self.weights[4] - 0.1*(y - t) * features[3]
-        w5 = self.weights[5] - 0.1*(y - t) * features[4]
-        w6 = self.weights[6] - 0.1*(y - t) * features[5]
+        w1 = self.weights[1] - 0.1*(y - t) * float(features[0])
+        w2 = self.weights[2] - 0.1*(y - t) * float(features[1])
+        w3 = self.weights[3] - 0.1*(y - t) * float(features[2])
+        w4 = self.weights[4] - 0.1*(y - t) * float(features[3])
+        w5 = self.weights[5] - 0.1*(y - t) * float(features[4])
+        w6 = self.weights[6] - 0.1*(y - t) * float(features[5])
         self.weights = [w0, w1, w2, w3, w4, w5, w6]
 
     def predict(self, features):
         sum = 0
         for i in range(len(self.weights)):
-            sum += features[i] * self.weights
+            sum += float(features[i]) * self.weights[i]
         if (sum <= 0):
             return 7
         else: return 9
+    
+    def test(self):
+        #gotta run the test images on the neuron
+        return 0
             
     
 if __name__=='__main__':
